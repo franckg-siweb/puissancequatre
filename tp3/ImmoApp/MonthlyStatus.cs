0aa70b1a51cbc64ff4fce4a0274365bf2b74c993
Franck <gutmann.franck@outlook.fr>
2024-03-16 19:04:07 +0100

    test(ImmoLoan): add GetMonthlyStatus tests

---
+++ tp3/ImmoApp/MonthlyStatus.cs
@@ -0,0 +1,7 @@
+namespace ImmoApp;
+public class MonthlyStatus
+{
+    public int Month { get; set; }
+    public double Paid { get; set; }
+    public double Remaining { get; set; }
+}
--- tp3/ImmoTestApp/ImmoLoanTest.cs
+++ tp3/ImmoTestApp/ImmoLoanTest.cs
@@ -2,46 +2,120 @@ using System.Collections;
 
 namespace ImmoTestApp;
 
-public class ImmoLoanTestData : IEnumerable<object[]>
-{
-    public IEnumerator<object[]> GetEnumerator()
-    {
-        yield return new object[] {
-            new ImmoLoan { 
-            Amount = 75000, 
-            Duration = 120, 
-            Rate = 0.5, 
-        }, 641
-        };
-        yield return new object[] {
-            new ImmoLoan { 
-            Amount = 100000, 
-            Duration = 180, 
-            Rate = 0.8, 
-        }, 590
-        };   
+public class ImmoLoanTestData {
+    public static ImmoLoan Data1 = new ImmoLoan { 
+        Amount = 75000, 
+        Duration = 120, 
+        Rate = 0.5, 
+    };
 
-         yield return new object[] {
-            new ImmoLoan { 
-            Amount = 200000, 
-            Duration = 240, 
-            Rate = 1.1, 
-        }, 929
-        };
-    }
+    public static ImmoLoan Data2 = new ImmoLoan { 
+        Amount = 100000, 
+        Duration = 180, 
+        Rate = 0.8, 
+    };
 
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    public static ImmoLoan Data3 = new ImmoLoan { 
+        Amount = 200000, 
+        Duration = 240, 
+        Rate = 1.1, 
+    };
+
 }
 
 public class ImmoLoanTest
 {
 
+    public class ImmoLoanMonthlyPaymentTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                ImmoLoanTestData.Data1, 641
+            };
+            yield return new object[] {
+                ImmoLoanTestData.Data2, 590
+            };   
+            yield return new object[] {
+                ImmoLoanTestData.Data2, 929
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
     [Theory]
-    [ClassData(typeof(ImmoLoanTestData))]
+    [ClassData(typeof(ImmoLoanMonthlyPaymentTestData))]
     public void ShouldComputeMonthlyPayment(ImmoLoan data, double expected)
     {
         Assert.Equal(expected, data.MonthlyPayment);
     }
 
+
+    public class ImmoLoanMonthlyStatusTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                ImmoLoanTestData.Data1, 1, new MonthlyStatus { 
+                    Month = 1, 
+                    Paid = 312.5, 
+                    Remaining = 74671.5 
+                }
+            };
+            yield return new object[] {
+                ImmoLoanTestData.Data2, 100, new MonthlyStatus { 
+                    Month = 100, 
+                    Paid = 173.33, 
+                    Remaining = 83000 
+                }
+            };   
+            yield return new object[] {
+                ImmoLoanTestData.Data2, 240, new MonthlyStatus { 
+                    Month = 240, 
+                    Paid = 929, 
+                    Remaining = 0 
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    [Theory]
+    [ClassData(typeof(ImmoLoanMonthlyStatusTestData))]
+    public void ShouldGetMonthlyStatus(ImmoLoan data, int month, MonthlyStatus expected)
+    {
+        // var result = data.GetMonthlyStatus(month);
+        // Assert.Equal(expected.Month, result.Month);
+        // Assert.Equal(expected.Paid, result.Paid);
+        // Assert.Equal(expected.Remaining, result.Remaining);
+    }
+
+
+    public class ImmoLoanInvalidMonthlyStatusTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] {
+                ImmoLoanTestData.Data1, -1
+            };
+            yield return new object[] {
+                ImmoLoanTestData.Data2, 181
+            };   
+            yield return new object[] {
+                ImmoLoanTestData.Data2, int.MaxValue
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    [Theory]
+    [ClassData(typeof(ImmoLoanInvalidMonthlyStatusTestData))]
+    public void ShouldNotGetMonthlyStatusWithInvalidMonth(ImmoLoan data, int month)
+    {
+        // Assert.Throws<ArgumentOutOfRangeException>(() => data.GetMonthlyStatus(month));
+    }
 
 }
